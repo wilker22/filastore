11955ed5b36dac775efa812e7b78dd9a61d9d9e7
wilker22 <wilker.silva@codevasf.gov.br>
2024-06-11 21:41:06 -0300

    primeiro commit

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Store>
+ */
+class StoreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $store = $this->faker->words(2, true);
+        return [
+            'name' => $this->faker->words(2,true),
+            'phone' => $this->faker->phoneNumber(),
+            'about' => $this->faker->paragraphs(2,true),
+            'slug' => str()->of($store)->slug()
+        ];
+    }
+}
