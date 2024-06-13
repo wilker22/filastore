17b1800c3eac39fec194bd43b5f85676ac67435d
wilker22 <wilker.silva@codevasf.gov.br>
2024-06-13 14:56:49 -0300

    mutate beforeFill e beforeSave para edição do preço

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -16,4 +16,24 @@ class EditProduct extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['price'] = number_format(
+            $data['price'] / 100,
+            2, ',','.'
+        );
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['price'] = (
+            (float) str_replace(
+            ['.',','], ['','.'], $data['price']
+            )) * 100;
+
+        return $data;
+    }
 }
