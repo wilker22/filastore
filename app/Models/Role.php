0ef8d71a039af90840d2ef26c77891f84b5c2a5a
wilker22 <wilker.silva@codevasf.gov.br>
2024-06-23 14:48:11 -0300

    model role!

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use App\Traits\BelongsToTenantTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as ModelsRole;
 
-class Role extends Model
+class Role extends ModelsRole
 {
     use HasFactory, BelongsToTenantTrait;
 }
